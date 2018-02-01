a01afb3cc7b9162b048420b7abe47d25e6db183b
pepesan <pepesan@gmail.com>
2018-02-01 16:07:23 +0100

    ejercicios de drag and drop

---
+++ ejercicios/drag.js
@@ -0,0 +1,14 @@
+/*
+1.- Crea un fichero HTML con un fichero JS asociado
+2.- Crea un div contenedor que tenag 3 divs dentro
+3.- Pinta los 3 divs para que quepan los tes a la vez a la misma altura y tengan fondo rojo
+4.- El primer div será arratrable, comprueba que se puede arrastrar
+5.- asigna el evento dragstart al primer div, asócialo con una función llamada empezando
+6.- dentro de la función empezando recuerda mandar un dato con el índice "iden" y con el valor del identificativo del primer div (setData)
+7.- Haz que cuando se haya entrado en el segundo div saque un mensaje por consola (arrastrando)
+8.- Haz que cuando se haya salido del segundo div saque un mensaje por consola
+9.- Haz que cuando se pasee sobre el tercer div anule el comportamiento por defecto (arrastrando)
+10.- Haz cuando se suelte el arrastrable (drop) en el tercer párrafo, anula el comportamiento por defecto y saca un mensaje por pantalla
+11.- En la función anterior saca por pantalla el identificativo del div que se haya mandado en el dragstart
+
+*/
